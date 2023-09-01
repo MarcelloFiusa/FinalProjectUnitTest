f890fe7c9a7733f2422136307b43bd6696fe0a47
Marcello Fiusa <marcellofiusa@gmail.com>
2023-09-01 18:13:03 -0300

    Final project

--- src/models/ice_cream_stand.py
+++ src/models/ice_cream_stand.py
@@ -4,7 +4,7 @@ from src.models.restaurant import Restaurant
 class IceCreamStand(Restaurant):
     """Um tipo especializado de restaurante."""
 
-    def __init__(self, restaurant_name, cuisine_type, flavors_list):
+    def __init__(self, restaurant_name, cuisine_type, flavors_list: list): #Melhoria - Inclusão da espera de passar uma lista
         """
         Inicialize os atributos da classe pai.
         Em seguida, inicialize os atributos específicos de uma sorveteria.
@@ -14,30 +14,43 @@ class IceCreamStand(Restaurant):
 
     def flavors_available(self):
         """Percorra a lista de sabores disponíveis e imprima."""
-        if self.flavors:
-            print("\nNo momento temos os seguintes sabores de sorvete disponíveis:")
+        if len(self.flavors) > 1:
+            msg = "No momento temos os seguintes sabores de sorvete disponíveis:"
             for flavor in self.flavors:
-                print(f"\t-{flavor}")
+                if flavor == self.flavors[-1]:
+                    msg = f'{msg} {flavor}'
+                    break
+                msg = f'{msg} {flavor},'
+            return msg
         else:
-            print("Estamos sem estoque atualmente!")
+            return "Estamos sem estoque atualmente!"
+
+        # MELHORIA - Criar lógica para entrar no if e, em caso de erro, no else
+        # MELHORIA - Criar lógica de concatenação da mensagem de retorno positiva
+        # BUG - Não entra no else pois é obrigado a passar algum valor para 'self.flavors'
+        # MELHORIA - substituir print
 
     def find_flavor(self, flavor):
         """Verifica se o sabor informado está disponível."""
-        if self.flavors:
-            if flavor in self.flavors:
-                print(f"Temos no momento {self.flavors}!")
-            else:
-                print(f"Não temos no momento {self.flavors}!")
+
+        if flavor in self.flavors:
+            return f"Temos no momento {flavor}!"
         else:
-            print("Estamos sem estoque atualmente!")
+            return f"Não temos no momento {flavor}!"
+
+        # BUG - Estava retornando a lista inteira e não somente o item pesquisado
+        # BUG - Não entra no else pois é obrigado a passar algum valor para flavors
+        # MELHORIA - substituir os prints por "return"
+        # MELHORIA - retirou o "if (self.flavors):" e "ELSE" para evitar duplicidade de retorno negativo
 
     def add_flavor(self, flavor):
         """Add o sabor informado ao estoque."""
-        if self.flavors:
-            if flavor in self.flavors:
-                print("\nSabor já disponivel!")
-            else:
-                self.flavors.append(flavor)
-                print(f"{flavor} adicionado ao estoque!")
+        if flavor in self.flavors:
+            return "Sabor já disponivel!"
         else:
-            print("Estamos sem estoque atualmente!")
+            self.flavors.append(flavor)
+            return f"{flavor} adicionado ao estoque!"
+
+        # MELHORIA - substituir print
+        # BUG - Não entra no else pois é obrigado a passar algum valor para flavors
+        # MELHORIA - Retirou o "if(self.flavors)" e "ELSE" pois a validação da função é apenas para a adição de sabores
--- src/models/restaurant.py
+++ src/models/restaurant.py
@@ -9,37 +9,60 @@ class Restaurant:
 
     def describe_restaurant(self):
         """Imprima uma descrição simples da instância do restaurante."""
-        print(f"Esse restaturante chama {self.cuisine_type} and serve {self.cuisine_type}.")
-        print(f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto.")
+
+        return f"Esse restaturante chama {self.restaurant_name} and serve {self.cuisine_type}. " \
+               f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto."
+
+        # BUG - A primeira informação do restaurante (nome) estava passando o que ele serve no lugar, repetindo a info.
+        # MELHORIA - substituir print
+        # MELHORIA - Juntar ambas strings em uma só
 
     def open_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está aberto para negócios."""
+
         if not self.open:
-            self.open = False
-            self.number_served = -2
-            print(f"{self.restaurant_name} agora está aberto!")
+            self.open = True
+            self.number_served = 2
+            msg = f"{self.restaurant_name} agora está aberto!"
+            return self.open and msg
         else:
-            print(f"{self.restaurant_name} já está aberto!")
+            return f"{self.restaurant_name} já está aberto!"
+
+        # MELHORIA - substituir print
+        # MELHORIA - Retornar tanto o status de aberto quanto a mensagem proposta, para garantir ambos.
+        # BUG - Ao entrar no if (quando o self.open = False), ele estava mantendo =False, ao invés de colocar o =True.
+        # MELHORIA - Não faz sentido servir negativas pessoas. Substituição do sinal
 
     def close_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está fechado para negócios."""
+
         if self.open:
             self.open = False
             self.number_served = 0
-            print(f"{self.restaurant_name} agora está fechado!")
+            return f"{self.restaurant_name} agora está fechado!"
         else:
-            print(f"{self.restaurant_name} já está fechado!")
+            return f"{self.restaurant_name} já está fechado!"
+
+        # MELHORIA - substituir print
 
     def set_number_served(self, total_customers):
         """Defina o número total de pessoas atendidas por este restaurante até o momento."""
         if self.open:
             self.number_served = total_customers
+            return f"{total_customers} de pessoas atendidas por este restaurante até o momento"
         else:
-            print(f"{self.restaurant_name} está fechado!")
+            return f"{self.restaurant_name} está fechado!"
+
+        # MELHORIA - substituir print
+        # MELHORIA - Incluir retorno de Mensagem com o total de customers atendidos
 
     def increment_number_served(self, more_customers):
         """Aumenta número total de clientes atendidos por este restaurante."""
         if self.open:
-            self.number_served = more_customers
+            self.number_served += more_customers
+            return f"{self.number_served} de pessoas atendidas por este restaurante"
         else:
-            print(f"{self.restaurant_name} está fechado!")
+            return f"{self.restaurant_name} está fechado!"
+
+        # MELHORIA - substituir print
+        # BUG - A lógica aplicada no "if" estava sobrescrevendo o valor e não incrementando.
